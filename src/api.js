db208c2e2807bf5292eb0c07d68bad3f0d3e9234
Jeremie Aubeau <j.aubeau@cafeyn.co>
2022-10-12 20:25:10 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const express = require('express');
+const serverless = require('serverless-http');
+require('dotenv/config');
+
+const app = express();
+const router = express.Router();
+
+const twitterBaseUrl = 'https://api.twitter.com/2';
+const tweetsParams =
+  '?tweet.fields=author_id%2Cattachments%2Cid&max_results=5&media.fields=alt_text%2Cheight%2Curl&user.fields=name%2Cprofile_image_url%2Cusername&expansions=attachments.media_keys%2Cauthor_id';
+
+const headers = {
+  Authorization: `Bearer ${process.env.API_BEARER_TOKEN}`,
+};
+
+const api = async (url, options = {}) => {
+  return axios(url, {
+    headers,
+    ...options,
+  });
+};
+
+router.get('/users/:id/tweets', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await api(
+      `${twitterBaseUrl}/users/${id}/tweets${tweetsParams}`,
+    );
+    res.json({ result: result.data });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.use(`/`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
