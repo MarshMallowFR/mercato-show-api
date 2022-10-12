ebbdae6cddff85506337951fbb8419b4a8a74425
Jeremie Aubeau <j.aubeau@cafeyn.co>
2022-10-12 21:45:02 +0200

    fix: add logs

--- src/api.js
+++ src/api.js
@@ -27,8 +27,7 @@ router.get('/users/:id/tweets', async (req, res) => {
     const result = await api(
       `${twitterBaseUrl}/users/${id}/tweets${tweetsParams}`,
     );
-    console.log(result);
-    res.status(200).json({ result: 'toto' });
+    res.status(200).json({ result: result.data });
   } catch (error) {
     res.status(500).json({ error });
   }
