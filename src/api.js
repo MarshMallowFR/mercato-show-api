3f0a52865b450e4c29583b1b26ff78e11238eff6
Jeremie Aubeau <j.aubeau@cafeyn.co>
2022-10-18 17:31:26 +0200

    feat: add params and other author

--- src/api.js
+++ src/api.js
@@ -10,11 +10,16 @@ app.use(cors());
 
 const twitterBaseUrl = 'https://api.twitter.com/2';
 const tweetsParams =
-  '?tweet.fields=author_id%2Cattachments%2Cid&max_results=5&media.fields=alt_text%2Cheight%2Curl&user.fields=name%2Cprofile_image_url%2Cusername&expansions=attachments.media_keys%2Cauthor_id';
+  '?tweet.fields=author_id%2Cattachments%2Cid%2Ccreated_at&max_results=50&media.fields=alt_text%2Cheight%2Curl&user.fields=name%2Cprofile_image_url%2Cusername%2Cverified&expansions=attachments.media_keys%2Cauthor_id&exclude=replies%2Cretweets';
 
 const tweeterHeaders = {
   Authorization: `Bearer ${process.env.API_BEARER_TOKEN}`,
 };
+
+const authorIds = [
+  '330262748', // Fabrizio Romano
+  '145684236', // Vincent Duluc
+];
 
 const api = async (url, options = {}) => {
   return axios(url, {
@@ -23,13 +28,36 @@ const api = async (url, options = {}) => {
   });
 };
 
-router.get('/users/:id/tweets', async (req, res) => {
+router.get('/users/tweets', async (req, res) => {
   try {
-    const { id } = req.params;
-    const result = await api(
-      `${twitterBaseUrl}/users/${id}/tweets${tweetsParams}`,
+    const promises = [];
+    for (const id of authorIds) {
+      promises.push(api(`${twitterBaseUrl}/users/${id}/tweets${tweetsParams}`));
+    }
+    const results = await Promise.all(promises);
+    console.log(results[0].data);
+    const allResults = results.reduce(
+      (acc, { data }) => {
+        const currData = data.data;
+        const currMedia = data.includes.media;
+        const currUsers = data.includes.users;
+        return {
+          data: acc?.data.concat(currData),
+          includes: {
+            media: acc.includes.media.concat(currMedia),
+            users: acc.includes.users.concat(currUsers),
+          },
+        };
+      },
+      {
+        data: [],
+        includes: {
+          media: [],
+          users: [],
+        },
+      },
     );
-    res.status(200).json({ result: result.data });
+    res.status(200).json({ result: allResults });
   } catch (error) {
     res.status(500).json({ error });
   }
